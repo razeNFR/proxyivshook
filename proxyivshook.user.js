ffcd09e9a9a49d64f90c417816fb2e3c76dca72e
razeN <71156828+razeNFR@users.noreply.github.com>
2026-09-09 11:17:20 +0200

    Bump version to 1.3.1 in user script

--- proxyivshook.user.js
+++ proxyivshook.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Twitch HLS Proxy v1.3.0
+// @name         Twitch HLS Proxy v1.3.1
 // @namespace    twitch-proxy-ivs
-// @version      1.3.0
+// @version      1.3.1
 // @author       razeNFR
 // @description  Twitch HLS via plusieurs proxys - Dashboard statistiques (nouvel onglet, design amélioré) + fallback automatique + résultats persistants + proxys personnalisés
 // @match        https://www.twitch.tv/*
@@ -24,7 +24,7 @@
     var UPDATE_CHECK_KEY = 'twitchProxyUpdateCheckV1';
 
     // Doit être tenu à jour avec le @version de l'en-tête du script.
-    var CURRENT_VERSION = '1.3.0';
+    var CURRENT_VERSION = '1.3.1';
 
     // Même URL que @updateURL : contient toujours la dernière version
     // publiée. On la relit nous-même (plutôt que de compter sur le
